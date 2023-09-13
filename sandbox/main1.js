2947920ddb75a2b513fea4efa11168fc91d77c52
ZorroSavage117 <zorrosavage117@byui.edu>
2023-09-13 11:49:25 -0600

    hi

---
+++ sandbox/main1.js
@@ -0,0 +1,10 @@
+console.log('this is my message as a developer');
+
+console.log(typeof 42);
+console.log(typeof "abc");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof { a: 1 });
+console.log(typeof [1, 2, 3]);
+console.log(typeof function hello() {});
